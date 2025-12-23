86587d48eb187ebd1cab4d993e558f921d5fe8b2
Miroslav Vyhnalik <miroslavvyhnalik@Mireks-MacBook-Pro.local>
2025-12-23 22:14:02 +0100

    First iteration of ChangingLayoutByText

--- ChangingLayoutByText/ChangingLayoutByText.lua
+++ ChangingLayoutByText/ChangingLayoutByText.lua
@@ -1,1 +1,36 @@
-print("Hello ChangingLayoutByText!")
+-- Register slash command: /l, /layout
+SLASH_MYLAYOUT1 = "/l"
+SLASH_MYLAYOUT2 = "/layout"
+
+local function ActivateLayoutByName(layoutName)
+    layoutName = layoutName:lower()
+
+    for layoutID, layoutInfo in pairs(EditModeManagerFrame:GetLayouts()) do
+        if layoutInfo.layoutName:lower() == layoutName then
+            C_EditMode.SetActiveLayout(layoutID)
+            print("Activated layout:", layoutInfo.layoutName)
+            return
+        end
+    end
+
+    print("Layout not found:", layoutName)
+end
+
+-- Slash command handler
+SlashCmdList["MYLAYOUT"] = function(msg)
+    msg = msg and msg:lower():trim() or ""
+
+    if msg == "help" then
+        print("Usage:")
+        local layouts = EditModeManagerFrame:GetLayouts()
+        for layoutID, layoutInfo in pairs(layouts) do
+            print("/l " .. layoutInfo.layoutName:lower() .. "  - activate " .. layoutInfo.layoutName .. " layout")
+        end
+        print("or")
+        for layoutID, layoutInfo in pairs(layouts) do
+            print("/layout " .. layoutInfo.layoutName:lower() .. "  - activate " .. layoutInfo.layoutName .. " layout")
+        end
+    else
+       ActivateLayoutByName(msg)
+    end
+end
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Collection of addons for World of Warcraft
 - [WhichDungeonInvite](https://github.com/slashik-mv/wow-addons/blob/main/_release/WhichDungeonInvite.zip): Prints which dungeon you got invited to (Premade Groups / M+)
 
 ## Planned
-- *ChangingLayoutByText*: Switching layouts from edit mode using predefined chat commands (e.g., /s normal activates the Normal layout)
+- [ChangingLayoutByText](https://github.com/slashik-mv/wow-addons/blob/main/_release/ChangingLayoutByText.zip): Switch layouts from Edit Mode using predefined chat commands: /l <layoutName> or /layout <layoutName> (e.g., /l normal activates the Normal layout). You can list all available commands by using /l help.
 - *CheckTalentsBeforeMplus*: Simply check that you’re running the correct talent build before starting a Mythic+ run
 
 ## Ideas
