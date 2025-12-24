10e902a4f5d8848f4058652d9a7332d4ac503e36
Miroslav Vyhnalik <miroslavvyhnalik@Mireks-MacBook-Pro.local>
2025-12-24 18:20:39 +0100

    Loading all talents build with names

--- CheckTalentsBeforeMplusOrRaid/CheckTalentsBeforeMplusOrRaid.lua
+++ CheckTalentsBeforeMplusOrRaid/CheckTalentsBeforeMplusOrRaid.lua
@@ -1,15 +1,41 @@
 print("Hello CheckTalentsBeforeMplusOrRaid!")
 
-local specID = GetSpecializationInfo(GetSpecialization())
-local configID = C_ClassTalents.GetActiveConfigID()
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
+f:RegisterEvent("TRAIT_CONFIG_UPDATED")
 
-if not configID then return end
+f:SetScript("OnEvent", function()       -- has to be called during some event because when game is still not fully loaded the PlayerUtil.GetCurrentSpecID() returns always 0
+    local specID = PlayerUtil.GetCurrentSpecID()
+    if specID == 0 then return end
 
-local loadoutIDs = C_ClassTalents.GetLoadoutIDsForConfig(configID)
+    print("Current specID:", specID)
 
-for _, loadoutID in ipairs(loadoutIDs) do
-    local info = C_ClassTalents.GetLoadoutInfo(loadoutID)
-    if info then
-        print(info.isActive and "► ACTIVE:" or "  ", info.name)
+    local configIDs1 = C_ClassTalents.GetConfigIDsBySpecID(specID)
+
+    print("#configIDs1: " .. #configIDs1)
+
+    for _, configID in ipairs(configIDs1) do
+        local info = C_Traits.GetConfigInfo(configID)
+        if info then
+            print(("Talent build: %s (configID=%d)"):format(info.name, configID))
+        end
+    end
+end)
+
+local function GetCurrentSpecIDSafe()       -- does not work at the end
+    local specID = PlayerUtil.GetCurrentSpecID()
+    if specID ~= 0 then
+        return specID
+    end
+
+    local specIndex = GetSpecialization()
+    if specIndex then
+        return GetSpecializationInfo(specIndex)
     end
 end
+
+local currentSpecID = GetCurrentSpecIDSafe()
+print("Current currentSpecID:", currentSpecID)
+
+
