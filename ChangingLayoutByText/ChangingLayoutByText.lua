20cb5163eeb9cc6a759b7929cbb0d17e7c91aa65
Miroslav Vyhnalik <miroslavvyhnalik@Mireks-MacBook-Pro.local>
2025-12-25 19:24:04 +0100

    ChangingLayoutByText adding msg in middle of the screen after switching layouts

--- ChangingLayoutByText/ChangingLayoutByText.lua
+++ ChangingLayoutByText/ChangingLayoutByText.lua
@@ -2,13 +2,41 @@
 SLASH_MYLAYOUT1 = "/l"
 SLASH_MYLAYOUT2 = "/layout"
 
+-- Simple BIG text in middle of the screen
+local function ShowBigTextInCenter(msg, duration)
+    -- creating frame for text
+    local frame = CreateFrame("Frame", nil, UIParent)
+    frame:SetSize(800, 200)
+    frame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+    frame:Hide()
+
+    -- setting text object
+    local text = frame:CreateFontString(nil, "OVERLAY", "GameFontNormalHuge")
+    text:SetPoint("CENTER")
+    text:SetText(msg)
+
+    -- custom size of text
+    local font, _, flags = text:GetFont()
+    text:SetFont(font, 40, flags)
+
+    frame.text = text
+    frame:Show()
+
+    C_Timer.After(duration or 10, function()
+        frame:Hide()
+    end)
+end
+
+-- Activate Layout by Name
 local function ActivateLayoutByName(layoutName)
     layoutName = layoutName:lower()
 
     for layoutID, layoutInfo in pairs(EditModeManagerFrame:GetLayouts()) do
         if layoutInfo.layoutName:lower() == layoutName then
             C_EditMode.SetActiveLayout(layoutID)
-            print("Activated layout:", layoutInfo.layoutName)
+            local msg = "Activated layout: " .. layoutInfo.layoutName
+            print(msg)
+            ShowBigTextInCenter(msg, 1)
             return
         end
     end
